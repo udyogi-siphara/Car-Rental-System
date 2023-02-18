a3ba78cad8958aacafb53cb5bea99baba5f357c5
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 20:55:16 -0800

    Payment Dto Added

---
+++ 02-Back-End/src/main/java/lk/ijse/spring/dto/PaymentDTO.java
@@ -0,0 +1,21 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Car _Rental_System
+ * Date        : 2/17/2023
+ * Time        : 8:53 PM
+ */
+
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class PaymentDTO {
+
+}
