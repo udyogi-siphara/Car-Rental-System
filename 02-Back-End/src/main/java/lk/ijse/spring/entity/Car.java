9c55271a6206d24a33744d8e23e425145dfeca96
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 20:11:25 -0800

    Added Car Entity

--- 02-Back-End/src/main/java/lk/ijse/spring/entity/Car.java
+++ 02-Back-End/src/main/java/lk/ijse/spring/entity/Car.java
@@ -7,6 +7,21 @@
 
 package lk.ijse.spring.entity;
 
-public class Car {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+public class Car {
+    @Id
+    private String adminId;
+    private String email;
+    private String userName;
+    private String password;
 }
