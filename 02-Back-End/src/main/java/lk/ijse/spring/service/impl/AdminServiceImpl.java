270d6524608fab7d79ad587dcbace5ed17aa57ce
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 22:35:50 -0800

    Added AdminServiceImpl

---
+++ 02-Back-End/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
@@ -0,0 +1,19 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Car _Rental_System
+ * Date        : 2/17/2023
+ * Time        : 10:31 PM
+ */
+
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.dto.AdminDTO;
+import lk.ijse.spring.service.AdminService;
+
+public class AdminServiceImpl implements AdminService {
+
+    @Override
+    public AdminDTO checkAdminLogIn(String id, String password) {
+        return null;
+    }
+}
