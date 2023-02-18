9462fa8adc6fc202283aa49c5884e664ff2bceba
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 20:31:58 -0800

    Added Admin Entity

---
+++ 02-Back-End/src/main/java/lk/ijse/spring/entity/Admin.java
@@ -0,0 +1,27 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Car _Rental_System
+ * Date        : 2/17/2023
+ * Time        : 8:27 PM
+ */
+
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+public class Admin {
+    @Id
+    private String adminId;
+    private String email;
+    private String userName;
+    private String password;
+}
