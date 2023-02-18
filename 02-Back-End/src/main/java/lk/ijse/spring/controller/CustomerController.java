e6173cb6934df9a7a8e5747ade25f62f91578c9e
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 22:11:03 -0800

    Added Customer Controller

---
+++ 02-Back-End/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,19 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Car _Rental_System
+ * Date        : 2/17/2023
+ * Time        : 10:10 PM
+ */
+
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/customer")
+public class CustomerController {
+
+}
