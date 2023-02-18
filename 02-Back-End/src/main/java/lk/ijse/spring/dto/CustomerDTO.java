93ceceb137e9aaf203e7963fd4b34a5d4252851d
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 20:44:27 -0800

    Customer Dto added

---
+++ 02-Back-End/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -0,0 +1,25 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Car _Rental_System
+ * Date        : 2/17/2023
+ * Time        : 8:39 PM
+ */
+
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CustomerDTO {
+    private String name;
+    private String nic;
+    private String address;
+    private String email;
+    private String image;
+}
