f9719e76f5c08c9612e1c39d94cd676bb51453bd
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 23:04:20 -0800

    Create Payment Service Impl

---
+++ 02-Back-End/src/main/java/lk/ijse/spring/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,36 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Car _Rental_System
+ * Date        : 2/17/2023
+ * Time        : 11:03 PM
+ */
+
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.dto.PaymentDTO;
+import lk.ijse.spring.service.PaymentService;
+
+import java.util.List;
+
+public class PaymentServiceImpl implements PaymentService {
+
+    @Override
+    public String generateReservationBillIdId() {
+        return null;
+    }
+
+    @Override
+    public void makePaymentForReservation(PaymentDTO paymentDTO) {
+
+    }
+
+    @Override
+    public String getIncomeByDate(String type, String start_date, String end_date) {
+        return null;
+    }
+
+    @Override
+    public List<PaymentDTO> getTodayIncomeList() {
+        return null;
+    }
+}
