cde56db89c5b30a01322f2ac6a6b99bd4fccfc96
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 22:09:25 -0800

    Car Controller Added

---
+++ 02-Back-End/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -0,0 +1,20 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Car _Rental_System
+ * Date        : 2/17/2023
+ * Time        : 10:08 PM
+ */
+
+package lk.ijse.spring.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/car")
+public class CarController {
+
+}
