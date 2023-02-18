1e513096320215d47d0292fa258a76d8f00ebed0
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 22:14:11 -0800

    Added Controller clz

--- 02-Back-End/src/main/java/lk/ijse/spring/controller/LoginController.java
+++ 02-Back-End/src/main/java/lk/ijse/spring/controller/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/driver")
-public class DriverController {
+@RequestMapping("/login")
+public class LoginController {
 
 }
