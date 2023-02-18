b522c52f738c8df7029646d368e627b6dacd07d1
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 20:57:55 -0800

    Added DriverScheduleDTO

---
+++ 02-Back-End/src/main/java/lk/ijse/spring/dto/DriverScheduleDTO.java
@@ -0,0 +1,21 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Car _Rental_System
+ * Date        : 2/17/2023
+ * Time        : 8:56 PM
+ */
+
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class DriverScheduleDTO {
+
+}
