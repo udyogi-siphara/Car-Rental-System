d9f25ca17492f0913fcb81f2a78a55d81c93ef8a
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 22:58:57 -0800

    Added CarServiceImpl

---
+++ 02-Back-End/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
@@ -0,0 +1,71 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Car _Rental_System
+ * Date        : 2/17/2023
+ * Time        : 10:56 PM
+ */
+
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.service.CarService;
+
+import java.util.List;
+
+public class CarServiceImpl implements CarService {
+
+    @Override
+    public void saveCar(CarDTO carDTO) {
+        
+    }
+
+    @Override
+    public void updateCar(CarDTO carDTO) {
+
+    }
+
+    @Override
+    public void deleteCar(String id) {
+
+    }
+
+    @Override
+    public CarDTO getCarDetail(String id) {
+        return null;
+    }
+
+    @Override
+    public List<CarDTO> getAllCarDetail() {
+        return null;
+    }
+
+    @Override
+    public List<CarDTO> getCarsUnderMaintain() {
+        return null;
+    }
+
+    @Override
+    public List<CarDTO> getCarsNeedMaintain() {
+        return null;
+    }
+
+    @Override
+    public List<CarDTO> getUnavailableOrAvailableCarsByStatus(String status) {
+        return null;
+    }
+
+    @Override
+    public List<CarDTO> getAvailableAndRentalCarsForReservation(String pick_up_date, String return_date, String status) {
+        return null;
+    }
+
+    @Override
+    public void setCarStatusUnavailableOrAvailable(String id, String status) {
+
+    }
+
+    @Override
+    public List<CarDTO> sortCarsByAttributes(CarDTO carDTO) {
+        return null;
+    }
+}
