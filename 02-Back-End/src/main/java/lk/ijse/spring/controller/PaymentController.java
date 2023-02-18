1da1c4037f7b5e563e82ffb2cc1123533c5f4076
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-17 22:16:12 -0800

    create payment controller

---
+++ 02-Back-End/src/main/java/lk/ijse/spring/controller/PaymentController.java
@@ -0,0 +1,19 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: Car _Rental_System
+ * Date        : 2/17/2023
+ * Time        : 10:11 PM
+ */
+
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/payment")
+public class PaymentController {
+
+}
