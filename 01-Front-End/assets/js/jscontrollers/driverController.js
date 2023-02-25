c07582502a47b9389f56d8714645fc8a569ea8b2
udyogi-siphara <udyogisiphara22@gmail.com>
2023-02-24 23:01:01 -0800

    View All Drivers

--- 01-Front-End/assets/js/jscontrollers/driverController.js
+++ 01-Front-End/assets/js/jscontrollers/driverController.js
@@ -12,15 +12,15 @@ function saveDriver(){
         driverId: $("#save-driver-Id").val(),
         name: $("#save-driver-nic").val(),
         nic: $("#save-driver-name").val(),
-        dob: $("#save-driver-address").val(),
-        address: $("#save-driver-mobile").val(),
+        dob: $("#save-driver-dob").val(),
+        address: $("#save-driver-address").val(),
         drivingLicenseNumber: $("#save-driver-license").val(),
 
     }
 
     $.ajax({
         url: baseUrl + "driver",
-        method: "POST",
+        method: "post",
         contentType: "application/json",
         data: JSON.stringify(driverDTO),
         success: function (res){
@@ -34,7 +34,26 @@ function saveDriver(){
             alert(ob.responseJSON.message);
             console.log(ob.responseJSON.message)
         }
-    })
+    });
+}
 
-    clearDriversform();
+function loadAllDrivers(){
+    $("#admin-all-drivers-table").empty();
+
+    $.ajax({
+        url: baseUrl + "driver/allDriverDetail",
+        method: "GET",
+        success: function (resp) {
+            for (const driver of resp.data) {
+                let row = `<tr><td>${driver.driverNic}</td><td>${driver.driver_name}</td><td>${driver.address}</td><td>${driver.mobile}</td><td>${driver.join_date}</td></tr>`;
+                $("#admin-all-drivers-table").append(row);
+
+                $("#admin-all-drivers-table>tr").off("click");
+                $("#admin-all-drivers-table>tr").click(function () {
+                    driver_nic = $(this).children(":eq(0)").text();
+                    $("#admin-driver-viewDetailsBtn").prop('disabled', false);
+                });
+            }
+        }
+    });
 }
