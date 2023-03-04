de3564ef03203f7a7533296647e9ba6942db2fc2
udyogi-siphara <udyogisiphara22@gmail.com>
2023-03-04 01:03:56 -0800

    Content updated

--- 01-Front-End/assets/js/jscontrollers/customerController.js
+++ 01-Front-End/assets/js/jscontrollers/customerController.js
@@ -195,7 +195,6 @@ function updateCustomer(){
     });
 }
 
-
 function viewAllCustomer(){
     $("#customer-view-table").empty();
 
@@ -219,5 +218,245 @@ function viewAllCustomer(){
         }
     });
 }
+
+
+
+
+
+/*VALIDATIONS*/
+//validation start
+
+const cusNICRegEx = /^[0-9/A-z]{10,15}$/;
+const cusDrivingRegEx = /^[0-9/A-z]{9}$/;
+const cusNameRegEx = /^[A-z ]{2,20}$/;
+const cusAddressRegEx = /^[0-9/A-z. ,]{7,}$/;
+const cusContactRegEx = /^[0-9]{3}[-]?[0-9]{7}$/;
+const cusEmailRegEx = /^[a-z0-9]{3,}[@]?[a-z]{1,}[.]?[a-z]{2,}$/;
+const cusPasswordRegEx = /^[a-z0-9]{6}$/;
+const cusUseNameRegEx = /^[a-z ]{6}$/;
+
+
+$('#name-signup,#email-signup,#nic-signup,#address-signup,#contact-signup,#driving-license-signup,#username-signup,#password-signup').on('keydown', function (eventOb) {
+    if (eventOb.key == "Tab") {
+        eventOb.preventDefault(); // stop execution of the button
+    }
+});
+
+$('#name-signup,#email-signup,#nic-signup,#address-signup,#contact-signup,#driving-license-signup,#username-signup,#password-signup').on('blur', function () {
+    formValid();
+});
+
+
+$("#name-signup").on('keyup', function (eventOb) {
+    setButton();
+
+
+});
+$("#email-signup").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfCustValid();
+    }
+});
+
+$("#address-signup").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfCustValid();
+    }
+});
+
+$("#contact-signup").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfCustValid();
+    }
+});
+
+$("#driving-license-signup").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfCustValid();
+    }
+});
+
+$("#username-signup").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfCustValid();
+    }
+});
+$("#password-signup").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfCustValid();
+    }
+});
+
+// focusing events end
+$("#btnInSignupSpa").attr('disabled', true);
+
+function clearAll() {
+    $('#name-signup,#email-signup,#nic-signup,#address-signup,#contact-signup,#driving-license-signup,#username-signup,#password-signup').val("");
+    $('#name-signup,#email-signup,#nic-signup,#address-signup,#contact-signup,#driving-license-signup,#username-signup,#password-signup').css('border', '2px solid #ced4da');
+    $('#name-signup').focus();
+    $("#btnInSignupSpa").attr('disabled', true);
+
+    /*$("#lblDriverNIC,#lblDriverName,#lblDriverAdress,#lblDriverContact,#lblDriverEmail").text("");*/
+
+}
+
+function formValid() {
+    var cusName = $("#name-signup").val();
+    $("#name-signup").css('border', '2px solid green');
+    $("#lblCusName").text("");
+    if (cusNameRegEx.test(cusName)) {
+        var cusEmail = $("#email-signup").val();
+        if (cusEmailRegEx.test(cusEmail)) {
+            $("#email-signup").css('border', '2px solid green');
+            $("#lblCusEmail").text("");
+            var cusNic = $("#nic-signup").val();
+            if (cusNICRegEx.test(cusNic)) {
+                $("#nic-signup").css('border', '2px solid green');
+                $("#lblCusNic").text("");
+                var cusAddress = $("#address-signup").val();
+                if (cusAddressRegEx.test(cusAddress)) {
+                    $("#address-signup").css('border', '2px solid green');
+                    $("#lblCusAddress").text("");
+                    var carContact = $("#contact-signup").val();
+                    if (cusContactRegEx.test(carContact)) {
+                        $("#contact-signup").css('border', '2px solid green');
+                        $("#lblCusContact").text("");
+                        var carDrivingLice = $("#driving-license-signup").val();
+                        if (cusDrivingRegEx.test(carDrivingLice)) {
+                            $("#driving-license-signup").css('border', '2px solid green');
+                            $("#lblCusLicense").text("");
+                            var carUser = $("#username-signup").val();
+                            if (cusUseNameRegEx.test(carUser)) {
+                                $("#username-signup").css('border', '2px solid green');
+                                $("#lblCusUserName").text("");
+
+
+                                var cusPassword = $("#password-signup").val();
+                                if (cusPasswordRegEx.test(cusPassword)) {
+                                    $("#password-signup").css('border', '2px solid green');
+                                    $("#lblCusPassword").text("");
+                                    return true;
+                                } else {
+                                    $("#password-signup").css('border', '2px solid red');
+                                    $("#lblCusPassword").text("Invalid Please Enter Again(Ex :Please must be include only letters and numbers at least 6");
+                                    return false;
+                                }
+                            } else {
+                                $("#username-signup").css('border', '2px solid red');
+                                $("#lblCusUserName").text("Invalid Please Enter Again(Ex :Please must be include only letters at least 6");
+                                return false;
+                            }
+                        } else {
+                            $("#driving-license-signup").css('border', '2px solid red');
+                            $("#lblCusLicense").text("Invalid Please Enter Again(Ex : 45645645L");
+                            return false;
+                        }
+                    } else {
+                        $("#contact-signup").css('border', '2px solid red');
+                        $("#lblCusContact").text("Invalid Please Enter Again(Ex :0714524520)");
+                        return false;
+                    }
+                } else {
+                    $("#address-signup").css('border', '2px solid red');
+                    $("#lblCusAddress").text("Invalid Please Enter Again(Ex : 02,Bibile)");
+                    return false;
+                }
+            } else {
+                $("#nic-signup").css('border', '2px solid red');
+                $("#lblCusNic").text("Invalid Please Enter Again(Ex : 200108202209 / 20010820220V)");
+                return false;
+            }
+        } else {
+            $("#email-signup").css('border', '2px solid red');
+            $("#lblCusEmail").text("Invalid Please Enter Again(Ex : udyogi12@gmail.com)");
+            return false;
+        }
+    } else {
+        $("#name-signup").css('border', '2px solid red');
+        $("#lblCusName").text("Invalid Please Enter Again(Ex : Udyogi)");
+        return false;
+    }
+}
+
+function checkIfCustValid() {
+    var cusName = $("#name-signup").val();
+    if (cusNameRegEx.test(cusName)) {
+        $("#email-signup").focus();
+        var cusEmail = $("#email-signup").val();
+        if (cusEmailRegEx.test(cusEmail)) {
+            $("#nic-signup").focus();
+            var cusNic = $("#nic-signup").val();
+            if (cusNICRegEx.test(cusNic)) {
+                $("#address-signup").focus();
+                var cusAddress = $("#address-signup").val();
+                if (cusAddressRegEx.test(cusAddress)) {
+                    $("#contact-signup").focus();
+                    var cusContact = $("#contact-signup").val();
+                    if (cusContactRegEx.test(cusContact)) {
+                        $("#driving-license-signup").focus();
+                        var cusDrivingLic = $("#driving-license-signup").val();
+                        if (cusDrivingRegEx.test(cusDrivingLic)) {
+                            $("#username-signup").focus();
+                            var cusUser = $("#username-signup").val();
+                            if (cusUseNameRegEx.test(cusUser)) {
+                                $("#password-signup").focus();
+                                var cusPassword = $("#password-signup").val();
+                                if (cusPasswordRegEx.test(cusPassword)) {
+                                    var resp = cusPasswordRegEx.test(cusPassword);
+                                    if (resp) {
+                                        if ($("#register-form-NIC-image-signup").val() == "") {
+                                            alert("Please Upload Driver License Image.....")
+                                        } else {
+                                            registerCustomer();
+                                            clearAll();
+                                        }
+                                    }
+                                } else {
+                                    $("#password-signup").focus();
+                                }
+                            } else {
+                                $("#username-signup").focus();
+                            }
+                        } else {
+                            $("#driving-license-signup").focus();
+                        }
+                    } else {
+                        $("#contact-signup").focus();
+                    }
+                } else {
+                    $("#address-signup").focus();
+                }
+
+            } else {
+                $("#nic-signup").focus()
+            }
+        } else {
+            $("#email-signup").focus();
+        }
+    } else {
+        $("#name-signup").focus();
+    }
+
+}
+
+function setButton() {
+    let b = formValid();
+    if (b) {
+        $("#btnInSignupSpa").attr('disabled', false);
+    } else {
+        $("#btnInSignupSpa").attr('disabled', true);
+    }
+}
+
+$('#btnInSignupSpa').click(function () {
+    checkIfCustValid();
+});
+
 
 
